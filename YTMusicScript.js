3267f689393471d0a1526ee2900b0debdbe43a50
Tsunami014 <max.worrall@education.nsw.gov.au>
2024-04-19 17:55:40 +1000

    Made some adjustments

--- YTMusicScript.js
+++ YTMusicScript.js
@@ -1,94 +1,10 @@
-const context = {
-    "context": {
-        "client": {
-            "clientName": "WEB_REMIX",
-            "clientVersion": "1." + new Date().toISOString().split('T')[0].replace(/-/g, '') + ".01.00",
-        },
-        "user": {},
-    }
-}
-
 const USER_AGENT_TABLET = "Mozilla/5.0 (iPad; CPU OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1";
 
-YTM_DOMAIN = "https://music.youtube.com"
-YT_DOMAIN = "https://www.youtube.com"
-YTM_WATCH_URL = YTM_DOMAIN + "/watch?v="
-YTM_BASE_API = YT_DOMAIN + "/youtubei/v1/"
-YTM_PARAMS = "?alt=json"
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
-
-const headers = {
-    "user-agent": USER_AGENT,
-    "accept": "*/*",
-    "accept-encoding": "gzip, deflate",
-    "content-type": "application/json",
-    "content-encoding": "gzip",
-    "origin": YTM_DOMAIN,
-}
-
-const Client = http.newClient(true);
-
-function send_request(endpoint, body, additionalParams = "") {
-    Object.assign(body, context);
-    let headers = {"Accept-Language": "en-US", "Cookie": "PREF=hl=en&gl=US" };
-    if(true) //useMobile
-		headers["User-Agent"] = USER_AGENT_TABLET;
-    
-        const resp = http.POST("https://www.youtube.com/youtubei/v1/search?alt=json", JSON.stringify({'query': 'Never gonna give you up', 'context': {'client': {'clientName': 'WEB_REMIX', 'clientVersion': '1.20240415.01.00'}, 'user': {}}}), headers, false);
-        return JSON.parse(resp.body);
-
-    const response = Client.POST(
-        "https://www.youtube.com/youtubei/v1/search?alt=json",
-        {'query': 'Never gonna give you up', 'context': {'client': {'clientName': 'WEB_REMIX', 'clientVersion': '1.20240415.01.00'}, 'user': {}}}.toString(),
-        {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0', 'accept': '*/*', 'accept-encoding': 'gzip, deflate', 'content-type': 'application/json', 'content-encoding': 'gzip', 'origin': 'https://music.youtube.com'}
-    );
-
-    // const response = Client.POST(
-    //     YTM_BASE_API + endpoint + YTM_PARAMS + additionalParams,
-    //     JSON.stringify(body),
-    //     headers,
-    //     true
-    // );
-
-    return response;
-
-    const responseText = JSON.parse(response.text);
-
-    if (response.status >= 400) {
-        const message = "Server returned HTTP " + response.status + ": " + response.reason + ".\n";
-        const error = responseText.error ? responseText.error.message : '';
-        throw new Error(message + error);
-    }
-
-    return responseText;
-}
-
-source.enable = function (conf) {
-    /**
-     * @param conf: SourceV8PluginConfig (the SomeConfig.js)
-     */
-    /*const response = send_request("search", {"query": "Never gonna give you up"});
-    const resp = response.contents.tabbedSearchResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents.slice(1).reduce((acc, i) => {
-        const title = i.musicShelfRenderer.title.runs[0].text;
-        acc[title] = i.musicShelfRenderer.contents;
-        return acc;
-    }, {});
-
-    const end = resp['Songs'].map(i => {
-        const info = i.musicResponsiveListItemRenderer.flexColumns;
-        return {
-            Name: info[0].musicResponsiveListItemFlexColumnRenderer.text.runs[0].text,
-            Author: info[1].musicResponsiveListItemFlexColumnRenderer.text.runs[0].text,
-            Album: info[1].musicResponsiveListItemFlexColumnRenderer.text.runs[2].text,
-            Duration: info[1].musicResponsiveListItemFlexColumnRenderer.text.runs[4].text,
-            Plays: info[2].musicResponsiveListItemFlexColumnRenderer.text.runs[0].text,
-            URL: YTM_WATCH_URL + i.musicResponsiveListItemRenderer.playlistItemData.videoId,
-            ID: i.musicResponsiveListItemRenderer.playlistItemData.videoId
-        };
-    });*/
-    return streams('lYBUbBu4W08');
-    return end;
-}
+const YTM_DOMAIN = "https://music.youtube.com"
+const YT_DOMAIN = "https://www.youtube.com"
+const YTM_WATCH_URL = YTM_DOMAIN + "/watch?v="
+const YTM_BASE_API = YT_DOMAIN + "/youtubei/v1/"
+const YTM_PARAMS = "?alt=json"
 
 const info = {
     'context': {
@@ -102,6 +18,34 @@ const info = {
         'User-Agent': 'com.google.android.apps.youtube.music/'
     },
     'api_key': 'AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8'
+}
+
+const ctx = {
+    "context": {
+        "client": {
+            "clientName": "WEB_REMIX",
+            "clientVersion": "1." + new Date().toISOString().split('T')[0].replace(/-/g, '') + ".01.00",
+        },
+        "user": {},
+    }
+}
+
+const Client = http.newClient(true);
+
+function send_request(endpoint, body, additionalParams = "") {
+    Object.assign(body, ctx);
+    let headers = {"Accept-Language": "en-US", "Cookie": "PREF=hl=en&gl=US" };
+    if(true) //useMobile
+		headers["User-Agent"] = USER_AGENT_TABLET;
+        //TODO: Make this like it should be
+        const resp = http.POST("https://www.youtube.com/youtubei/v1/search?alt=json", JSON.stringify({'query': 'Never gonna give you up', 'context': {'client': {'clientName': 'WEB_REMIX', 'clientVersion': '1.20240415.01.00'}, 'user': {}}}), headers, false);
+        return JSON.parse(resp.body);
+}
+
+source.enable = function (conf) {
+    /**
+     * @param conf: SourceV8PluginConfig (the SomeConfig.js)
+     */
 }
 
 function executeRequest(url, headers = {}, data = null) {
